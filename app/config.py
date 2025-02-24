685e9be92603ba0790a84f20efdb953a0b36def6
Mair nousH <themeynoush@gmail.com>
2025-02-24 11:32:56 +0100

    style: reformat app/config.py

--- app/config.py
+++ app/config.py
@@ -11,13 +11,14 @@ class Config:
     Configuration class for Flask application.
     Pulls in configuration from environment variables (12-factor compliance).
     """
+
     DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///weather.db")
 
     DEBUG = False
     TESTING = False
     # A secret key (needed for sessions in Flask, if used)
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev")  # 'dev' as default for development
-    
+
     # Weather API specific config:
     WEATHER_API_KEY = os.environ.get(
         "WEATHER_API_KEY"
@@ -25,7 +26,7 @@ class Config:
     WEATHER_API_URL = os.environ.get(
         "WEATHER_API_URL", "http://api.weatherapi.com/v1/current.json"
     )
-    
+
     # TODO:
     # Other configs like database URL could be here as needed
 
